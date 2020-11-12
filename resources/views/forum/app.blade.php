226c916948fc383b57b0dcd893726bbbad69f1da
MyukaQA <mylian214@gmail.com>
2020-11-12 14:05:21 +0700

    Update app.blade.php

--- resources/views/forum/app.blade.php
+++ resources/views/forum/app.blade.php
@@ -24,7 +24,7 @@
         <div class="row">
           <div class="col-6">
             <li class="nav-item active">
-              <a class="nav-link text-white" href="#">Dashboard<span class="sr-only">(current)</span></a>
+              <a class="nav-link text-white" href="{{route('dashboard-user')}}">Dashboard<span class="sr-only">(current)</span></a>
             </li>
           </div>
           <div class="col-6 text-right">

c91f3c8e5207a305af43b03b481ed2f4d8a0d5b3
MyukaQA <mylian214@gmail.com>
2020-12-13 18:37:54 +0700

    Update KendalaController.php

--- app/Http/Controllers/KendalaController.php
+++ app/Http/Controllers/KendalaController.php
@@ -19,8 +19,6 @@ class KendalaController extends Controller
     }
 
     public function validasi(Request $request){
-        // $data = \App\Kendala::all();
-        // return view('dashboard.hasil', ['data' => $data]);
         $validator = Validator::make($request->all(),[
             'daun' => 'required',
             'lumut' => 'required',
@@ -32,17 +30,9 @@ class KendalaController extends Controller
             return back();
         }
 
-        $validasi = new ValidasiKendala;
-        $validasi->daun = $request->daun;
-        $validasi->lumut = $request->lumut;
-        $validasi->air = $request->air;
-
-        $validasi->save();
-
         // all
         if ($request->daun == 'daunYes' && $request->lumut == 'lumutYes' && $request->air == 'airYes'){
             $data = Kendala::all();
-            DB::table('validasi_kendala')->truncate();
             toast('Penanganan Berhasil Muncul', 'success')->autoClose(3000);
         }
         // all
@@ -50,17 +40,14 @@ class KendalaController extends Controller
         // Fokus Daun
         elseif ($request->daun == 'daunYes' && $request->lumut == 'lumutNo' && $request->air == 'airNo'){
             $data = Kendala::where('ciri2', 'daun')->get();
-            DB::table('validasi_kendala')->truncate();
             toast('Penanganan Daun Berhasil Muncul', 'success')->autoClose(3000);
         }
         elseif ($request->daun == 'daunYes' && $request->lumut == 'lumutYes' && $request->air == 'airNo'){
             $data = Kendala::where('ciri2', 'daun')->orWhere('ciri2', 'lumut')->get();
-            DB::table('validasi_kendala')->truncate();
             toast('Penanganan Daun dan Lumut Berhasil Muncul', 'success')->autoClose(3000);
         }
         elseif ($request->daun == 'daunYes' && $request->lumut == 'lumutNo' && $request->air == 'airYes'){
             $data = Kendala::where('ciri2', 'daun')->orWhere('ciri2', 'air')->get();
-            DB::table('validasi_kendala')->truncate();
             toast('Penanganan Daun dan Air Berhasil Muncul', 'success')->autoClose(3000);
         }
         // Fokus Daun
@@ -68,12 +55,10 @@ class KendalaController extends Controller
         // Fokus Lumut
         elseif ($request->daun == 'daunNo' && $request->lumut == 'lumutYes' && $request->air == 'airNo'){
             $data = Kendala::where('ciri2', 'lumut')->get();
-            DB::table('validasi_kendala')->truncate();
             toast('Penanganan Lumut Berhasil Muncul', 'success')->autoClose(3000);
         }
         elseif ($request->daun == 'daunNo' && $request->lumut == 'lumutYes' && $request->air == 'airYes'){
             $data = Kendala::where('ciri2', 'lumut')->orWhere('ciri2', 'air')->get();
-            DB::table('validasi_kendala')->truncate();
             toast('Penanganan Lumut dan Air Berhasil Muncul', 'success')->autoClose(3000);
         }
         // Fokus Lumut
@@ -81,7 +66,6 @@ class KendalaController extends Controller
         // Fokus Air
         elseif ($request->daun == 'daunNo' && $request->lumut == 'lumutNo' && $request->air == 'airYes'){
             $data = Kendala::where('ciri2', 'air')->get();
-            DB::table('validasi_kendala')->truncate();
             toast('Penanganan Air Berhasil Muncul', 'success')->autoClose(3000);
         }
         // Fokus Air
@@ -89,104 +73,16 @@ class KendalaController extends Controller
         // Fokus Air
         elseif ($request->daun == 'daunNo' && $request->lumut == 'lumutNo' && $request->air == 'airNo'){
             $data = Kendala::where('ciri2', 'no')->get();
-            DB::table('validasi_kendala')->truncate();
             toast('Mohon Maaf Penanganan Tidak Ada', 'warning')->autoClose(3000);
         }
         // Fokus Air
 
-        
         return view('dashboard/hasil', compact('data'));
     }
 
-    // public function hasil(){
-    //     $semua = ValidasiKendala::
-    //     where('daun', 'daunYes')
-    //     ->orWhere('lumut', 'lumutYes')
-    //     ->orWhere('air', 'airYes')
-    //     ->get(); 
-
-    //     $daunaja = ValidasiKendala::
-    //     where('daun', 'daunYes')
-    //     ->orWhere('lumut', 'lumutNo')
-    //     ->orWhere('air', 'airNo')
-    //     ->get();
-
-    //     if ($semua){
-    //         $data = Kendala::all();
-    //         DB::table('validasi_kendala')->truncate();
-            
-    //     }elseif ($daunaja){
-    //         $data = Kendala::where('ciri2', 'daun')->get();
-    //         // DB::table('validasi_kendala')->truncate();
-
-    //     }
-    //     // elseif (\App\ValidasiKendala::where([
-    //     //     ['daun', 'daunYes'],
-    //     //     ['lumut', 'lumutYes'],
-    //     //     ['air', 'airNo']
-    //     // ])->get()){
-    //     //     $data = \App\Kendala::where('ciri2', 'daun')->orWhere('ciri2', 'lumut')->get();
-    //     //     // DB::table('validasi_kendala')->truncate();
-        
-    //     // }elseif (\App\ValidasiKendala::where([
-    //     //     ['daun', 'daunYes'],
-    //     //     ['lumut', 'lumutNo'],
-    //     //     ['air', 'airYes']
-    //     // ])->get()){
-    //     //     $data = \App\Kendala::where('ciri2', 'daun')->orWhere('ciri2', 'air')->get();
-    //     //     // DB::table('validasi_kendala')->truncate();
-        
-    //     // }elseif (\App\ValidasiKendala::where([
-    //     //     ['daun', 'daunNo'],
-    //     //     ['lumut', 'lumutYes'],
-    //     //     ['air', 'airNo']
-    //     // ])->get()){
-    //     //     $data = \App\Kendala::where('ciri2', 'lumut')->get();
-    //     //     // DB::table('validasi_kendala')->truncate();
-        
-    //     // }
-    //     // // if (\App\ValidasiKendala::where([
-    //     // //     ['daun', 'daunYes'],
-    //     // //     ['lumut', 'lumutYes'],
-    //     // //     ['air', 'airNo']
-    //     // // ])->get()){
-    //     // //     $data = \App\Kendala::where('ciri2', 'daun')->orWhere('ciri2', 'lumut')->get();
-    //     // //     // DB::table('validasi_kendala')->truncate();
-        
-    //     // // }
-    //     // elseif (\App\ValidasiKendala::where([
-    //     //     ['daun', 'daunNo'],
-    //     //     ['lumut', 'lumutYes'],
-    //     //     ['air', 'airYes']
-    //     // ])->get()){
-    //     //     $data = \App\Kendala::where('ciri2', 'lumut')->orWhere('ciri2', 'air')->get();
-    //     //     // DB::table('validasi_kendala')->truncate();
-        
-    //     // }elseif (\App\ValidasiKendala::where([
-    //     //     ['daun', 'daunNo'],
-    //     //     ['lumut', 'lumutNo'],
-    //     //     ['air', 'airYes']
-    //     // ])->get()){
-    //     //     $data = \App\Kendala::where('ciri2', 'air')->get();
-    //     //     // DB::table('validasi_kendala')->truncate();
-        
-    //     // }
-
-
-    //     return view('dashboard/hasil', compact('data'));
-    // }
-
     public function back(){
-        
-        // DB::table('validasi_kendala')->delete();
         return view('dashboard/kendala');
     }
-
-    // public function cek(Request $request){
-    //     $kendala = New Kendala;
-    //     $kendala->ciri2 = $request->daun;
-    //     $kendala->penanganan = $request->penanganan;
-    // } 
 
     public function create(Request $request){
         $validator = Validator::make($request->all(),[

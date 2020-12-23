baf20b0a4ab4b6162676734aed94770da89a4392
MyukaQA <mylian214@gmail.com>
2020-12-23 12:18:46 +0700

    Update KendalaTableSeeder.php

--- database/seeds/KendalaTableSeeder.php
+++ database/seeds/KendalaTableSeeder.php
@@ -13,17 +13,17 @@ class KendalaTableSeeder extends Seeder
     {
         \App\Kendala::create([
             'ciri2' => "daun",
-            'penanganan'  => 'daun konten',
+            'penanganan'  => 'Solusinya adalah dengan memasang jaring peneduh dengan intensitas 60 % pada bagian atas. Hal ini akan meminimalisir tanaman agar tidak berlebihan mendapatkan sinar matahari disaat cuaca yang terik.',
         ]);
 
         \App\Kendala::create([
             'ciri2' => "lumut",
-            'penanganan'  => 'lumut konten',
+            'penanganan'  => 'Cara membersihkan lumut yang mudah adalah&nbsp; merendam selang plastik ke dalam air panas, lalu menambahkan satu sendok makan pemutih per galon sebagai pembersih. Selanjutnya, jalankan seperti biasanya (setelah perendaman) jika menggunakan sistem NFT. Hal tersebut untuk membersihkan lumut&nbsp; yang tumbuh di dalam selang yang sulit dijamah. ',
         ]);
 
         \App\Kendala::create([
             'ciri2' => "air",
-            'penanganan'  => 'air konten',
+            'penanganan'  => 'Tanaman hidrponik memerlukan pasokan air yang sesuai aturan. Idealnya adalah debit air 1-2 liter/menit.',
         ]);
     }
 }
